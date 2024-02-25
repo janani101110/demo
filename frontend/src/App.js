59ac94b451215cff04a70676f7dcb63bd3f2593b
janani <darmasiripkdjl.21@uom.lk>
2024-02-25 13:00:47 +0530

    first commit frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+import {Navbar} from './Component/Navbar/Navbar';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import {Home} from './Pages/Home/Home';
+import {Resources} from './Pages/Resources/Resources';
+import {Writepost} from './Pages/Resources/Writepost';
+import {Sensors} from './Pages/Resources/Sensors/Sensors';
+import {MotionSen} from './Pages/Resources/Sensors/MotionSen';
+import {Projects} from './Pages/Projects/Projects';
+import {Blogs} from './Pages/Blogs/Blogs';
+import {Shopping} from './Pages/Shopping/Shopping';
+import {Forum} from './Pages/Forum/Forum';
+import {AboutUs} from './Pages/AboutUs/AboutUs';
+import { Footer } from './Component/Footer/Footer';
+import {Shoppingpost} from './Pages/Shoppingpost/Shoppingpost';
+import SignUp from './Pages/LogIn/SignUp';
+import Login from './Pages/LogIn/LogIn';
+import { UserContextProvider } from './Context/UserContext';
+
+
+function App() {
+  return (
+    <div>
+      <UserContextProvider>
+      <BrowserRouter>
+      <Navbar/>
+      
+      <Routes>
+          <Route path='/home'element={<Home/>}/>
+          <Route path='/resources'element={<Resources/>}/>
+            <Route path='/writepost'element={<Writepost/>}/>
+            <Route path='/sensors'element={<Sensors/>}/>
+              <Route path='/MotionSen'element={<MotionSen/>}/>
+           
+          <Route path='/projects'element={<Projects/>}/>
+          <Route path='/blogs'element={<Blogs/>}/>
+          <Route path='/shopping'element={<Shopping/>}/>
+          <Route path='/forum'element={<Forum/>}/>
+          <Route path='/aboutus'element={<AboutUs/>}/>
+          <Route path='/signup'element={<SignUp/>}/>
+          <Route path='/login'element={<Login/>}/>
+          <Route path='/shoppingpost' element={<Shoppingpost/>}/>
+
+          <Route path='/' element={<Navigate to="/home" />}/>
+          
+      </Routes>
+      
+      <Footer/>
+      </BrowserRouter>
+      </UserContextProvider>
+      
+
+    </div>
+  );
+}
+
+
+export default App;
---
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+//import {FaBars} from "react-icons/fa";
+import logo from '../Assets/logo.png'
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../Context/UserContext';
+
+
+export const Navbar = () => {
+  const [menu,setMenu] = useState("home");
+
+  const{user}=useContext(UserContext)
+  console.log(user)
+  return (
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt="" className='logo'/>
+            
+            <h1>Gavesha</h1>
+        </div>
+        <ul className='nav-menu'>
+            <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration: 'none'}} to='/home'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("resources")}}><Link style={{textDecoration: 'none'}} to='/resources'>Resources</Link>{menu==="resources"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("projects")}}><Link style={{textDecoration: 'none'}} to='/projects'>Projects</Link>{menu==="projects"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("blogs")}}><Link style={{textDecoration: 'none'}} to='/blogs'>Blogs</Link>{menu==="blogs"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shopping")}}><Link style={{textDecoration: 'none'}} to='/shopping'>Shopping</Link>{menu==="shopping"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("forum")}}><Link style={{textDecoration: 'none'}} to='/forum'>Forum</Link>{menu==="forum"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("about")}}><Link style={{textDecoration: 'none'}} to='/aboutus'>About Us</Link>{menu==="about"?<hr/>:<></>}</li>
+        </ul>'
+        <div className='nav-login'>
+            <Link to='/signup'><button>Sign Up</button></Link>
+            <Link to='/login'><button>Sign In</button></Link>
+        </div>
+      
+    </div>
+
+  )
+}
+export default Navbar;
