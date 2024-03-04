8cf2a82ae48ab8542d2d613349f9241f57f51e7a
SewminiAmanda <110555098+SewminiAmanda@users.noreply.github.com>
2024-03-04 10:23:27 +0530

    blog frontend changes

--- frontend/src/Pages/Blogs/WriteBlog.jsx
+++ frontend/src/Pages/Blogs/WriteBlog.jsx
@@ -3,14 +3,18 @@ import "./Blog.css";
 export const WriteBlog = () => {
     return (
     <div className="createBlog"> 
-   <div className="CreateBlogTextdiv">
-  </div>
   <div className="CreateBlogTextdiv">
   <h1 className='createBlogTitle'>Create a Blog Post</h1>
         <form className='createBlogFormBody'>
+          <label className='createBlogTextLabel'> Title: </label>
+          <br/>
           <input type="text" placeholder='Enter post title' className='createBlogTextbox'/>
           <br/>
+          <label className='createBlogTextLabel'> Image: </label>
+          <br/>
           <input type="file"  className='createBlogEnterImage'/>
+          <br/>
+          <label className='createBlogTextLabel'> Blog Body: </label>
           <br/>
           <textarea rows={30} cols={30} className='createBlogTextArea' placeholder='Enter post body'/>
           <br/>
